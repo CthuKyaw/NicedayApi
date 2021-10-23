046693fbc3d9ac980eb1fad11fe2c02506f48b3c
Dolphin <cthukyaw13@gmail.com>
2021-10-23 17:02:30 +0630

    Added error handling in user.controller

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -14,117 +14,170 @@ const { genSaltSync, hashSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 module.exports = {
     createUser: (req, res) => {
-        const body = req.body;
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
-        create(body, (err, results) => {
-            if (err) {
-                //console.log(err);
+        try{
+            const body = req.body;
+            const salt = genSaltSync(10);
+            body.password = hashSync(body.password, salt);
+            create(body, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.json({
+                        success: 0,
+                        message: err
+                    });
+                }
+    
                 return res.json({
-                    success: 0,
-                    message: err
+                    success: 1,
+                    message: "Account successfully created.",
+                    data: results
                 });
-            }
-
-            return res.json({
-                success: 1,
-                message: "Account successfully created.",
-                data: results
             });
-        });
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
+            });
+        }
+        
     },
     getUsers: (req, res) => {
-
-        getUsers((err, results) => {
-            if (err) {
-                //console.log(err);
-                return res.send(500).json({
-                    success: 0,
-                    message: "Failed to fetch users"
+        try{
+            getUsers((err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Failed to fetch users"
+                    });
+                }
+                return res.status(200).json({
+                    success: 1,
+                    data: results
                 });
-            }
-            return res.status(200).json({
-                success: 1,
-                data: results
+            })
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
             });
-        })
+        }
+        
     },
     getUserById: (req, res) => {
-        const data = req.body;
-        getUserById(data, (err, results) => {
-            if (err) {
-                //console.log(err);
-                return res.send(403).json({
-                    success: 0,
-                    message: "Failed to fetch user"
+        try{
+            const data = req.body;
+            getUserById(data, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.send(403).json({
+                        success: 0,
+                        message: "Failed to fetch user"
+                    });
+                }
+                return res.status(200).json({
+                    data: results[0]
                 });
-            }
-            return res.status(200).json({
-                data: results[0]
+            })
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
             });
-        })
+        }
+        
     },
     updateUser: (req, res) => {
-        const data = req.body;
-        const salt = genSaltSync(10);
-        data.password = hashSync(data.password, salt);
-        updateUser(data, (err, results) => {
-            if (err) {
-                //console.log(err);
-                return res.send(500).json({
-                    success: 0,
-                    message: "Update error"
+        try{
+            const data = req.body;
+            const salt = genSaltSync(10);
+            data.password = hashSync(data.password, salt);
+            updateUser(data, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Update error"
+                    });
+                }
+                return res.status(200).json({
+                    success: 1,
+                    message: "Updated successfully"
                 });
-            }
-            return res.status(200).json({
-                success: 1,
-                message: "Updated successfully"
             });
-        });
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
+            });
+        }
+        
     },
     deleteUser: (req, res) => {
-        const data = req.body;
-        deleteUser(data.id, (err, results) => {
-            if (err) {
-                //console.log(err);
-                return res.send(500).json({
-                    success: 0,
-                    message: "Cannot find requested user to delete"
+        try{
+            const data = req.body;
+            deleteUser(data.id, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Cannot find requested user to delete"
+                    });
+                }
+                if (!results) {
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Failed to delete user"
+                    });
+                }
+                return res.status(200).json({
+                    success: 1,
+                    message: "User Removed"
                 });
-            }
-            if (!results) {
-                return res.send(500).json({
-                    success: 0,
-                    message: "Failed to delete user"
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                message: "User Removed"
+            })
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
             });
-        })
+        }
+        
     },
     suspendOrUnsuspendUser: (req, res) => {
-        const data = req.body;
-        suspendOrUnsuspendUser(data, (err, results) => {
-            if (err) {
-                //console.log(err);
-                return res.send(500).json({
-                    success: 0,
-                    message: "Cannot find requested user"
+        try{
+            const data = req.body;
+            suspendOrUnsuspendUser(data, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Cannot find requested user"
+                    });
+                }
+                if (!results) {
+                    return res.send(500).json({
+                        success: 0,
+                        message: `${data.active == 2?'Unable to suspend user':'Unable to un-suspend user'}`
+                    });
+                }
+                return res.status(200).json({
+                    success: 1,
+                    message: `${data.active == 2?'User Unsuspended':'User Suspended (This user will not be able to login!)'}`
                 });
-            }
-            if (!results) {
-                return res.send(500).json({
-                    success: 0,
-                    message: `${data.active == 2?'Unable to suspend user':'Unable to un-suspend user'}`
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                message: `${data.active == 2?'User Unsuspended':'User Suspended (This user will not be able to login!)'}`
+            })
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
             });
-        })
+        }
+        
     },
     login: (req, res) => {
         const data = req.body;
@@ -173,38 +226,56 @@ module.exports = {
 
     },
     getUserWorkout: (req, res) => {
-        const data = req.query;
-        getWorkoutData(data,(err, results) => {
-            if (err) {
-
-                return res.send(500).json({
-                    success: 0,
-                    message: "Failed to fetch workout data"
+        try{
+            const data = req.query;
+            getWorkoutData(data,(err, results) => {
+                if (err) {
+    
+                    return res.send(500).json({
+                        success: 0,
+                        message: "Failed to fetch workout data"
+                    });
+                }
+                return res.status(200).json({
+                    success: 1,
+                    rows:results
                 });
-            }
-            return res.status(200).json({
-                success: 1,
-                rows:results
+            })
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
             });
-        })
+        }
+        
     },
     createWorkoutRecord: (req, res) => {
-        const body = req.body;
-        createRecord(body, (err, results) => {
-            if (err) {
-                //console.log(err);
+        try{
+            const body = req.body;
+            createRecord(body, (err, results) => {
+                if (err) {
+                    //console.log(err);
+                    return res.json({
+                        success: 0,
+                        message: err
+                    });
+                }
+    
                 return res.json({
-                    success: 0,
-                    message: err
+                    success: 1,
+                    message: "Status updated.",
+                    data: results
                 });
-            }
-
-            return res.json({
-                success: 1,
-                message: "Status updated.",
-                data: results
             });
-        });
-    },
+        }
+        catch(e){
+            return res.json({
+                success: 0,
+                message: e
+            });
+        }
+        
+    }
     
 }
