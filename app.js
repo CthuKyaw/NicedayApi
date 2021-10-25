1c19336ed468a82f67fb6c8fe3bc632c78d7802b
Dolphin <cthukyaw13@gmail.com>
2021-10-25 19:26:26 +0630

    Modify Cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const userRouter = require("./api/users/user.router");
 
 app.use(express.json());
-app.use("/*", userRouter);
+app.use("/", userRouter);
 
 app.listen(process.env.APP_PORT,()=>{
   console.log("Server listening at :" + process.env.APP_PORT);
