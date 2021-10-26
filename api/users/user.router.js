15d5188534c9a038a34a266d1b9fb3e2784f7423
Dolphin <cthukyaw13@gmail.com>
2021-10-26 13:40:42 +0630

    Add error handling to router

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -24,16 +24,34 @@ router.use(cors({
 
 const { checkToken } = require("../../auth/token_validation");
 
+const logConfiguration = {
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            // Create the log directory if it does not exist
+            filename: '../../../logs/error.log'
+        })
+    ]
+};
 
-router.post("/users",createUser);
-router.get("/users/all",checkToken, getUsers);
-router.get("/users/single",checkToken, getUserById);
-router.patch("/users",checkToken, updateUser);
-router.delete("/users",checkToken, deleteUser);
-router.patch("/users/suspend",checkToken, suspendOrUnsuspendUser);
-router.post("/users/login", login);
-router.get("/workout",checkToken,getUserWorkout);
-router.post("/workout",checkToken,createWorkoutRecord);
+var logger = new winston.createLogger(logConfiguration);
+
+
+try{
+    router.post("/users",createUser);
+    router.get("/users/all",checkToken, getUsers);
+    router.get("/users/single",checkToken, getUserById);
+    router.patch("/users",checkToken, updateUser);
+    router.delete("/users",checkToken, deleteUser);
+    router.patch("/users/suspend",checkToken, suspendOrUnsuspendUser);
+    router.post("/users/login", login);
+    router.get("/workout",checkToken,getUserWorkout);
+    router.post("/workout",checkToken,createWorkoutRecord);
+}
+catch(e){
+    logger.error(`Error at user.router. \n ${e}`);
+}
+
 
 
 module.exports = router;
