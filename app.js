6cf3f165068b7abacb12bc2165a4c55174b6db39
Dolphin <cthukyaw13@gmail.com>
2021-10-28 18:49:09 +0630

    Add socket.io connection for video chat

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
 const userRouter = require("./api/users/user.router");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin:`${process.env.CORS_CLIENT_HOST}`,
+    methods:["GET","POST"]
+	}
+});
+
 
 app.use(express.json());
 app.use(userRouter);
 
-app.listen(process.env.APP_PORT,()=>{
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(process.env.APP_PORT,()=>{
   console.log("Server listening at :" + process.env.APP_PORT);
 });
 
