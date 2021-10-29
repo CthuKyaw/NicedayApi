b1ff5ece3aababa1af4cd7e45057340f0e98dc67
Dolphin <cthukyaw13@gmail.com>
2021-10-30 05:16:33 +0630

    Removed Socket Io from api

--- app.js
+++ app.js
@@ -16,33 +16,10 @@ const server = https.createServer(options,app);
 const userRouter = require("./api/users/user.router");
 const cors = require("cors");
 
-const io = require("socket.io")(server, {
-	path: "/socket",
-	cors: {
-		origin:`${process.env.CORS_CLIENT_HOST}`,
-    methods:["GET","POST"]
-	}
-});
-
 app.use("api/*",cors());
 app.use(express.json());
 app.use(userRouter);
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
 
 server.listen(process.env.APP_PORT,()=>{
   console.log("Server listening at :" + process.env.APP_PORT);
